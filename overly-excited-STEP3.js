86735a6649ebdf377710daf6a418f0b25aa30e57
daniel-certa-1228 <mfdancerta@gmail.com>
2017-07-13 09:02:51 -0500

    almost done

--- overly-excited-STEP3.js
+++ overly-excited-STEP3.js
@@ -1,4 +1,4 @@
-console.log("overly-excited-STEP3.js");
+console.log("overly-excited-STEP2.js");
 
 // Create an addExcitement function that should console.log() rows of words. It should take an array containing the words of a sentence and output them in the developer console.
 
@@ -21,24 +21,21 @@ function addExcitement(wordArray) {
 
 	let sentence1 = "";
 	let addExcitement = "!"
-	let space = " "
-	let excitementCount = 1;
 
 	for (let i = 0; i < wordArray.length; i++) {
 		let nextWord1 = wordArray[i];
 		let nextWord1WithSpace = wordArray[i] + ' ';
+
+		if ( (i+1)%3 === 0) {
+
+			sentence1 += nextWord1 + addExcitement;
+			addExcitement += addExcitement
 		
+		}  else  {
 
-		sentence1 += nextWord1WithSpace;
+			sentence1 += nextWord1WithSpace;
 
-		if ((i+1)%3 === 0) {
-			for (let j = 0; j < excitementCount; j++) {
-				sentence1 += addExcitement;
-			}
-			
 		}
-
-		excitementCount++;
 
 		console.log(sentence1);
 
